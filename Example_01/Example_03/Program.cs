1ffe8527df655ef7fcc103f6e833d8b425f6a983
maks11455 <maks101330@mail.ru>
2023-10-04 21:07:49 +0000

    first commit

---
+++ Example_01/Example_03/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите длину массива: "); 
+        if (int.TryParse(Console.ReadLine(), out int length) && length > 0) 
+        { 
+            int[] array = new int[length]; 
+ 
+            for (int i = 0; i < length; i++) 
+            { 
+                Console.Write($"Введите элемент {i + 1}: "); 
+                if (int.TryParse(Console.ReadLine(), out int element)) 
+                { 
+                    array[i] = element; 
+                } 
+                else 
+                { 
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число."); 
+                    i--; // Повторно запросить элемент 
+                } 
+            } 
+ 
+            Console.WriteLine("Введенный массив:"); 
+            for (int i = 0; i < length; i++) 
+            { 
+                Console.WriteLine($"Элемент {i + 1}: {array[i]}"); 
+            } 
+        } 
+        else 
+        { 
+            Console.WriteLine("Некорректный ввод длины массива. Пожалуйста, введите положительное целое число."); 
+        }
